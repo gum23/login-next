84d3bdf9055775a694afe027c04b26624a67c983
gum23 <gum23coder@gmail.com>
2025-11-09 23:40:41 -0300

    Correccion conexion mongodb

--- src/lib/dbconnect.js
+++ src/lib/dbconnect.js
@@ -1,14 +1,20 @@
-'use server'
+
 import mongoose from "mongoose";
 import config from "@/utils/config";
 
 
-const URI = `mongodb+srv://${config.mongodb_user}:${config.mongodb_password}@${config.mongodb_cluster}/${config.mongodb_name}?retryWrites=true&w=majority&appName=${config.mongodb_app}`;
+const URI = `mongodb+srv://${config.mongodb_user}:${config.mongodb_password}@${config.mongodb_cluster}/${config.mongodb_name}?retryWrites=true&w=majority`;
+
+let isConnected = false;
 
 const db = async () => { 
-  
+  if (isConnected) return;
+
   try {
-    const db = await mongoose.connect(URI);
+    const db = await mongoose.connect(URI, {
+      serverSelectionTimeoutMS: 5000, // corta más rápido si no se conecta
+    });
+    isConnected = db.connections[0].readyState;
     console.log("Database is conected to: ", db.connection.name)
   } catch (error) {
     console.log("The connection to database is broken ", error)
